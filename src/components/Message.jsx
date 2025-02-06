10e3163cd85bd3902aa2f87c714e1136328bb9f5
Sarah Koch <sarahkoch810@gmail.com>
2025-02-06 01:04:24 -0800

    Add PropTypes validation to Message component and set default type

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,6 +1,13 @@
-const Message = ({ type, children }) => {
+import PropTypes from 'prop-types';
+
+const Message = ({ type = 'info', children }) => {
   if (!children) return null;
   return <p className={`message ${type}`}>{children}</p>;
+};
+
+Message.PropTypes = {
+  type: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default Message;
